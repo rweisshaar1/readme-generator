0040443d1afd70a9824c6d7452b222c0192e68d5
rweisshaar1 <robbieweisshaar@gmail.com>
2023-07-25 16:09:24 -0500

    added some code to index.js file.

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    input: 'type',
+    message: 'What would you like the title of the README to be?',
+    name: 'title'
+  },
+  {
+    input: 'type',
+    message: 'What would you like the description to say?',
+    name: 'description'
+  },
+  
+];
+
+inquirer .prompt(questions)
+.then(writeToFile(`${questions.title}`, ))
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(`${fileName}.md`, data,(err)=>
+  err ? console.error(err) : console.log('Success!'))
+}
 
 // TODO: Create a function to initialize app
 function init() {}
